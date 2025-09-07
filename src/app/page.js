5674b95441198ca8ad45bf67fc173938fcec9ca1
nainaasharmaa <124238995+nainaasharmaa@users.noreply.github.com>
2025-09-07 19:38:19 +0530

    Add auth pages for Naina

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,51 @@
+import Image from "next/image";
+import styles from "./styles/home.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Smart Inventory System</h1>
-      <Link href="/login">Go to Login</Link> <br />
-      <Link href="/signup">Go to Signup</Link>
+    <div className={styles.container}>
+      {/* Navbar */}
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>
+          <Image src="/logo.jpg" alt="Asserta Logo" width={120} height={100} />
+          
+        </div>
+        <ul className={styles.navLinks}>
+          <li>About</li>
+          <li>Features</li>
+          <li>Contact</li>
+          <li>
+            <Link href="/signup">
+              <button className={styles.loginBtn}>Login / Signup</button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Hero Section */}
+      <main className={styles.hero}>
+        <div className={styles.heroLeft}>
+          <h1>Asserta</h1>
+          <h3>Your Assets, Always in Sight .</h3>
+          <p>
+            Transform the way you track, manage, and optimize your digital and physical assets with intelligent automation and real-time insights.
+          </p>
+          <div className={styles.buttons}>
+            <button className={styles.primaryBtn}>Get Started</button>
+            <button className={styles.secondaryBtn}>Learn More</button>
+          </div>
+        </div>
+
+        <div className={styles.heroRight}>
+          <div className={styles.circle}></div>
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className={styles.footer}>
+        <p>© 2025 Asserta. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
