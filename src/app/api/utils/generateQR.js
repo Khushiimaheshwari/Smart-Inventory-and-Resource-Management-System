218ca2bb21e40896b8282b6a1f8fc79fc388858a
Khushiimaheshwari <142376177+Khushiimaheshwari@users.noreply.github.com>
2025-11-24 23:17:07 +0530

    Final Code 3.0

--- src/app/api/utils/generateQR.js
+++ src/app/api/utils/generateQR.js
@@ -7,9 +7,17 @@ export async function generateQRCodeForAsset(assetId, baseUrl) {
     .populate("Lab_Name");
 
   if (!asset) throw new Error("Asset not found");
-  console.log(asset.PC_Name._id);
 
-  const qrValue = `${baseUrl}/adminPanel/asset_management/asset/${asset.PC_Name._id}`;
+  if (!asset.PC_Name._id) throw new Error("Asset PC reference missing");
+
+  const qrPayload = {
+    admin: `${baseUrl}/adminPanel/asset_management/asset/${asset.PC_Name._id}`,
+    faculty: `${baseUrl}/facultyPanel/allAssets/asset/${asset.PC_Name._id}`,
+    lab_technician: `${baseUrl}/lab_technicianPanel/asset_management/asset/${asset.PC_Name._id}`
+  };
+
+  const qrValue = JSON.stringify(qrPayload);
+
   const qrImage = await QRCode.toDataURL(qrValue);
 
   asset.QR_Code = qrImage;
