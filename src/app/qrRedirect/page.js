a6efd4978217e0c9ed1d525a3ccabb69cf2a2d7b
Khushi Maheshwari <142376177+Khushiimaheshwari@users.noreply.github.com>
2025-11-24 23:48:12 +0530

    Import useState in qrRedirect page component

--- src/app/qrRedirect/page.js
+++ src/app/qrRedirect/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { Loader2 } from "lucide-react";
 
